b6c7c7d698a8d8ef2f1fc08d5d215db303c6a681
annietse <annietse03@gmail.com>
2024-11-16 15:18:03 +0000

    feat: text to speech is working

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import SpeechToText from "@/components/stt";
+import TextToSpeech from "@/components/tts";
 
 export default function Home() {
   return (
     <div className="flex flex-col">
-      <SpeechToText />
+      <TextToSpeech />
     </div>
   );
 }
