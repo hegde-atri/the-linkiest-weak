0ade6628757aaad91977a8c295839097a904ae78
annietse <annietse03@gmail.com>
2024-11-16 16:07:47 +0000

    feat: separate function to take in msgs for tts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import SpeechToText from "@/components/stt";
 import TextToSpeech from "@/components/tts";
+import SpeechPlayer from "@/components/another_tts";
 
 export default function Home() {
   return (
     <div className="flex flex-col">
       <TextToSpeech />
+      <SpeechPlayer />
     </div>
   );
 }
