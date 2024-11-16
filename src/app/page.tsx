0eaa3b21310aa2c81acf88ce6cc788ea1bb5e5d4
hegde-atri <iamatrihegde@outlook.com>
2024-11-16 20:36:28 +0000

    feat: confetti

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import SpeechToText from "@/components/stt";
 import TextToSpeech from "@/components/tts";
 import SpeechPlayer from "@/components/another_tts";
+import ConfettiComponent from "@/components/confetti";
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center space-y-5">
+      <ConfettiComponent />
       <SpeechToText />
       <TextToSpeech />
       <SpeechPlayer />
